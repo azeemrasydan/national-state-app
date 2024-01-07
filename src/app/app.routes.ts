5fd2371b0e9fa1ef6236aae14e6f21ebf5d16a2e
Azeem Rasydan <azeem.rasydan@plus.com.my>
2024-01-07 20:48:29 +0800

    Added both root and home under construction notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RootComponent } from './pages/root/root.component';
 
 export const routes: Routes = [
+    {
+        title: 'Welcome',
+        path: '',
+        component: RootComponent
+    },
     {
         title: "Home",
         path: 'home',
